6d7fec4bedd0729e36464904f3a0cb89725752d6
Amer Ibric <ameribric@gmail.com>
2022-03-20 21:31:56 -0400

    commit tree e27ab1f456e8aed73435accb1af0ee6fae46eaba

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
-// Write your code here
+
+const num1 = 62
+const num2 = 1
+
+let multiply = (num1 * num2)
+console.log(num1 * num2)
+
+
+
+
+
+let random = Math.ceil(Math.random())
+function getRandom () {
+    return Math.random();
+}
+
+const num3 = 16
+const num4 = 6
+
+
+let mod = (num3%num4);
+function remainderNumber() {
+    return num3 % num4
+}
+console.log (num3 % num4)
+
+
+let max = Math.max(2, 3, 7, 20);
+function maxNumber() {
+    return Math.max()
+}
